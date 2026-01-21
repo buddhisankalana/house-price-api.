a4fcbb8fc1efa0dbb85b5eb3000077e6f0b69c2f
buddhisankalana <136596252+buddhisankalana@users.noreply.github.com>
2026-01-21 10:49:44 +0530

    Added garage feature

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ RUN pip install -r requirements.txt
 
 COPY main.py .
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "7860"]
--- main.py
+++ main.py
@@ -7,8 +7,10 @@ class HousePredictionInput(BaseModel):
     number_of_rooms: int
     location: str
     land_size_sqft: float
+    garage: int
 
 @app.post("/predict")
 def predict_price(item: HousePredictionInput):
-    estimated_price = (item.land_size_sqft * 200) + (item.number_of_rooms * 15000)
+    estimated_price = (item.land_size_sqft * 200) + (item.number_of_rooms * 15000) + \
+    (item.garage * 500000)
     return {"predicted_price": estimated_price}
